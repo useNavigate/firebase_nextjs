0b8e22a404e85ae9c522fa804bee429a02a5ae27
useNavigate <useNavigate@gmail.com>
2023-12-27 11:40:10 -0500

    first commit

---
+++ app/admin/page.js
@@ -0,0 +1,17 @@
+"use client";
+import React, { useEffect } from "react";
+import { useAuthContext } from "@/src/context/AuthContext";
+import { useRouter } from "next/navigation";
+
+function Page() {
+  const { user } = useAuthContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user == null) router.push("/");
+  }, [user]);
+
+  return <h1>Only logged in users can view this page </h1>;
+}
+
+export default Page;
